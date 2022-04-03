37ef8084bc35f58d8149257a72fb4765d4381364
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-04 01:45:02 +0200

    refactor: update the type of the failure on the validateNonNegativeDecimal's return signature

--- lib/src/non_negative_number/non_negative_number_validator.dart
+++ lib/src/non_negative_number/non_negative_number_validator.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter_clean_domain/flutter_clean_domain.dart';
 
 import 'non_negative_number_failure.dart';
 
-Either<ValueFailure<num>, num> validateNonNegativeDecimal(final num value) {
+Either<NonNegativeNumberFailure, num> validateNonNegativeDecimal(
+    final num value) {
   if (value < 0 || !value.isFinite) {
     return Left(NonNegativeNumberFailure(invalidValue: value));
   }
