2453f8ad291e9b97f21f4ede4d3e5ce226c2841f
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-04 14:06:02 +0200

    chore: add the example for the non negative numbers (#12)

---
+++ example/non_negative_number/non_negative_number_example.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_clean_common_value_objects/flutter_clean_common_value_objects.dart';
+
+main() {
+  final validNonNegativeNumber = NonNegativeNumber(10);
+  print(validNonNegativeNumber.value);
+  print(validNonNegativeNumber.isValid());
+  print(validNonNegativeNumber.getOrCrash());
+  final invalidNonNegativeNumber = NonNegativeNumber(-1);
+  print(invalidNonNegativeNumber.value);
+  print(invalidNonNegativeNumber.isValid());
+  try {
+    print(invalidNonNegativeNumber.getOrCrash());
+  } catch (e) {
+    print(e);
+  }
+}
